f1ad45247e74ae3905c9eb0d7c33e5e84f478c49
paramagicdev <konnor5456@gmail.com>
2018-05-12 04:14:09 -0400

    Recursive function works. Project complete.

--- fib_recurs.rb
+++ fib_recurs.rb
@@ -1,9 +1,13 @@
-def fibr(i)
-  i < 3 ? [0, i] : (fibr i - 1) << fibr(i - 1)[-1] + fibr(i - 2)[-1]
+def fib_recursive(num, num_sequence = [0, 1])
+  return num_sequence if num < 2
+
+  num_sequence << num_sequence[-1] + num_sequence[-2]
+  # pushes 0 + 1, then goes 1 + 1 etc
+  fib_recursive(num - 1, num_sequence)
 end
 
-p fibr(2) #=> 0, 1
-p fibr(1) #=> 0
-p fibr(3) #=> 0, 1 , 1
-p fibr(8) #=> 0, 1, 1, 2, 3, 5, 8, 13
-# p fibr(20)
+p fib_recursive(2) #=> 0, 1
+p fib_recursive(1) #=> 0
+p fib_recursive(3) #=> 0, 1 , 1
+p fib_recursive(8) #=> 0, 1, 1, 2, 3, 5, 8, 13
+p fib_recursive(20)
