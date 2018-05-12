3a90205b5f96ef5d8df8322c2b85f53139a20c13
konnor rogers <konnor7414@gmail.com>
2018-05-12 11:13:57 +0000

    Fixed recursive method

--- fib_recurs.rb
+++ fib_recurs.rb
@@ -1,11 +1,12 @@
 def fib_recursive(num, num_sequence = [0, 1])
-  return num_sequence if num < 2
+  return num_sequence.slice(0, num_sequence.length - 1) if num < 2
 
   num_sequence << num_sequence[-1] + num_sequence[-2]
   # pushes 0 + 1, then goes 1 + 1 etc
   fib_recursive(num - 1, num_sequence)
 end
 
+p fib_recursive(0)
 p fib_recursive(2) #=> 0, 1
 p fib_recursive(1) #=> 0
 p fib_recursive(3) #=> 0, 1 , 1
